a7aacfd095f4108670a4b861cd4d381f72c2c122
luminacious <67962264+luminacious@users.noreply.github.com>
2026-03-14 02:32:11 +0000

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material.icons:material-icons-extended:1.7.6")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.5")
